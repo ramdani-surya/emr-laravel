1533736665488849efa9ef941b4691dfeb4fbb70
Ramdani Surya M <a2.1800105@mhs.stmik-sumedang.ac.id>
2021-01-13 15:55:00 +0700

    edit kunjungan

--- app/Http/Controllers/VisitController.php
+++ app/Http/Controllers/VisitController.php
@@ -36,10 +36,13 @@ class VisitController extends Controller
      */
     public function create()
     {
+        $formTitle = 'Tambah Kunjungan';
+
         $data['subtitle']    = $this->subtitle;
-        $data['breadcrumbs'] = [$this->subtitle, 'Tambah Kunjungan'];
+        $data['breadcrumbs'] = [$this->subtitle, $formTitle];
         $data['polyclinics'] = Polyclinic::all();
         $data['patients']    = Patient::all();
+        $data['formTitle']   = $formTitle;
 
         return view('visit-add', $data);
     }
@@ -99,17 +102,6 @@ class VisitController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Visit  $visit
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Visit $visit)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Visit  $visit
@@ -117,7 +109,16 @@ class VisitController extends Controller
      */
     public function edit(Visit $visit)
     {
-        //
+        $formTitle = 'Edit Kunjungan';
+
+        $data['subtitle']    = $this->subtitle;
+        $data['breadcrumbs'] = [$this->subtitle, $formTitle];
+        $data['polyclinics'] = Polyclinic::all();
+        $data['patients']    = Patient::all();
+        $data['formTitle']   = $formTitle;
+        $data['visit']       = $visit;
+
+        return view('visit-add', $data);
     }
 
     /**
@@ -129,7 +130,48 @@ class VisitController extends Controller
      */
     public function update(Request $request, Visit $visit)
     {
-        //
+        $request->validate([
+            "patient"    => 'required|numeric|exists:patients,id',
+            "guarantator"          => [
+                'required',
+                Rule::in(config('constant.guarantator')),
+            ],
+            "member_number"       => 'required_if:guarantator,BPJS',
+            "access_rights"       => 'required_if:guarantator,BPJS|nullable|numeric|between:1,3',
+            "polyclinic"          => 'required|exists:polyclinics,id',
+            "registration_number" => "required|unique:visits,registration_number,$visit->id",
+        ]);
+
+        // buat data kunjungan
+        $visitData = $request->only(
+            "guarantator",
+            "member_number",
+            "access_rights",
+            "registration_number"
+        );
+        $visitData['patient_id'] = $request->patient;
+        $visitData['polyclinic_id'] = $request->polyclinic;
+
+        $visit->update($visitData);
+
+        // buat rekam medis untuk kunjungan yang telah dibuat
+        $visit->medicalRecord->update([
+            'patient_id' => $request->patient,
+        ]);
+
+        // buatkan antrean
+        $latestQueueNumber = Queue::whereDate('created_at', Carbon::today())
+            ->where('polyclinic_id', $visit->polyclinic_id)
+            ->max('queue_number');
+
+        $newQueueNumber = ($latestQueueNumber) ? $latestQueueNumber + 1 : 1;
+
+        $visit->queue->update([
+            'polyclinic_id' => $visit->polyclinic_id,
+            'queue_number'  => $newQueueNumber
+        ]);
+
+        return redirect(route('visits.index'));
     }
 
     /**
--- resources/views/visit-add.blade.php
+++ resources/views/visit-add.blade.php
@@ -7,18 +7,28 @@
 @section('content')
 <div class="row">
     <div class="col-md-12 card-box">
-        <form class="parsley-examples" action="{{ route('visits.store') }}" method="POST">
+        <form class="parsley-examples"
+            action="{{ !isset($visit) ? route('visits.store') : route('visits.update', $visit->id) }}" method="POST">
             @csrf
-            <h4 class="header-title">TAMBAH KUNJUNGAN</h4>
+            @if (isset($visit))
+            @method('put')
+            @endif
+
+            <h4 class="header-title">{{ $formTitle }}</h4>
             <p class="sub-header"></p>
 
             <div class="form-group clearfix">
-                <label for="patient" class="col-form-label control-label">Pasien<span class="text-danger">*</span></label>
+                <label for="patient" class="col-form-label control-label">Pasien<span
+                        class="text-danger">*</span></label>
                 <div>
                     <select class="form-control select2" id="patient" name="patient" required>
                         <option disabled selected>-- Masukkan NIK atau nama pasien --</option>
+                        @php
+                        $selectedPatient = !isset($visit) ? old('patient') : $visit->patient_id
+                        @endphp
+
                         @foreach ($patients as $patient)
-                        <option value="{{ $patient->id }}" @if (old('patient')===$patient->id) selected @endif>
+                        <option value="{{ $patient->id }}" @if ($selectedPatient===$patient->id) selected @endif>
                             {{ "$patient->nik | $patient->name" }}</option>
                         @endforeach
                     </select>
@@ -35,8 +45,12 @@
                 <div>
                     <select class="form-control select2" id="penjamin-input" name="guarantator" required>
                         <option disabled selected>-- Pilih penjamin --</option>
+                        @php
+                        $selectedGuarantator = !isset($visit) ? old('guarantator') : $visit->guarantator
+                        @endphp
+
                         @foreach (config('constant.guarantator') as $guarantator)
-                        <option value="{{ $guarantator }}" @if (old('guarantator')===$guarantator) selected @endif>
+                        <option value="{{ $guarantator }}" @if ($selectedGuarantator===$guarantator) selected @endif>
                             {{ $guarantator }}</option>
                         @endforeach
                     </select>
@@ -54,7 +68,7 @@
                             class="text-danger">*</span></label>
                     <div>
                         <input id="nomor-anggota" name="member_number" type="text" data-parsley-type="number"
-                            class="form-control" value="{{ old('member_number') }}">
+                            class="form-control" value="{{ !isset($visit) ? old('member_number') : $visit->member_number }}">
                     </div>
                     @error('member_number')
                     <ul class="parsley-errors-list filled">
@@ -67,7 +81,7 @@
                     <label class="control-label" for="hak-kelas">Hak Kelas<span class="text-danger">*</span></label>
                     <div>
                         <input id="hak-kelas" name="access_rights" type="number" min="1" max="3"
-                            data-parsley-type="number" class="form-control" value="{{ old('access_rights') }}">
+                            data-parsley-type="number" class="form-control" value="{{ !isset($visit) ? old('access_rights') : $visit->access_rights }}">
                     </div>
                     @error('access_rights')
                     <ul class="parsley-errors-list filled">
@@ -82,8 +96,12 @@
                 <div>
                     <select class="form-control select2" id="poli-tujuan" name="polyclinic" required>
                         <option disabled selected>-- Pilih poliklinik --</option>
+                        @php
+                        $selectedpolyclinic = !isset($visit) ? old('polyclinic') : $visit->polyclinic_id
+                        @endphp
+
                         @foreach ($polyclinics as $polyclinic)
-                        <option value="{{ $polyclinic->id }}" @if(old('polyclinic')===$polyclinic->id) selected
+                        <option value="{{ $polyclinic->id }}" @if($selectedpolyclinic===$polyclinic->id) selected
                             @endif>{{ $polyclinic->polyclinic_name }}</option>
                         @endforeach
                     </select>
@@ -100,7 +118,7 @@
                         class="text-danger">*</span></label>
                 <div>
                     <input type="text" class="form-control" id="nomor-registrasi" name="registration_number"
-                        value="{{ old('registration_number') }}" required>
+                        value="{{ !isset($visit) ? old('registration_number') : $visit->registration_number }}" required>
                 </div>
                 @error('registration_number')
                 <ul class="parsley-errors-list filled">
